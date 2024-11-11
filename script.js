44f0b9c0d3084a133d898be2e89046c08703d341
Sanjanpreet <157737848+Sanjanpreet@users.noreply.github.com>
2024-11-10 22:29:52 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+// Array created that contains the images and their properties
+const galleryItems = [
+    {
+        large: "images/flowers-yellow-large.jpg",
+        small: "images/flowers-yellow-small.jpg",
+        alt: "Sunflowers",
+        caption: "Sunflowers in the Dernekamp hamlet, Kirchspiel, Dülmen, North Rhine-Westphalia, Germany"
+    },
+    {
+        large: "images/flowers-red-large.jpg",
+        small: "images/flowers-red-small.jpg",
+        alt: "Poppies",
+        caption: "Poppies in cornfield, Dülmen, North Rhine-Westphalia, Germany"
+    },
+    {
+        large: "images/flowers-pink-large.jpg",
+        small: "images/flowers-pink-small.jpg",
+        alt: "Market",
+        caption: "Flower stand at the market in Münster, North Rhine-Westphalia, Germany"
+    },
+    {
+        large: "images/flowers-white-large.jpg",
+        small: "images/flowers-white-small.jpg",
+        alt: "Daffodils",
+        caption: "Daffodils in Sentmaring park, Münster, North Rhine-Westphalia, Germany"
+    },
+    {
+        large: "images/flowers-purple-large.jpg",
+        small: "images/flowers-purple-small.jpg",
+        alt: "Sentmaring Park",
+        caption: "Sentmaring Park, Münster, North Rhine-Westphalia, Germany"
+    }
+];
+
+
+// Select the thumbnails from the unordered list, tha main image and the caption 
+const thumbnailList = document.querySelector('#gallery ul');
+const featuredImage = document.querySelector('figure img');
+const figcaption = document.querySelector('figure figcaption');
+
+// Function to display a featured image based on the index
+function displayFeaturedImage(index) {
+    const item = galleryItems[index];
+    
+    // Set the featured image and caption
+    featuredImage.src = item.large;
+    featuredImage.alt = item.alt;
+    figcaption.textContent = item.caption;
+
+    // Update the thumbnail styles
+    document.querySelectorAll('.thumbnail').forEach((thumb, i) => {
+        thumb.classList.toggle('active', i === index);
+    });
+}
+
+// Populate the thumbnail list
+galleryItems.forEach((item, index) => {
+    const listItem = document.createElement('li');
+    const img = document.createElement('img');
+    
+    img.src = item.small;
+    img.alt = item.alt;
+    img.width = 240;
+    img.height = 160;
+    img.classList.add('thumbnail');
+
+    // Add a click event to update the featured image
+    img.addEventListener('click', () => displayFeaturedImage(index));
+
+    listItem.appendChild(img);
+    thumbnailList.appendChild(listItem);
+});
+
+// Display the first image by default
+displayFeaturedImage(0);
